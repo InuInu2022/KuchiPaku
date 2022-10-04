9cc03174abcb404dd66764b759ff187d6080fb96
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2022-10-05 00:26:52 +0900

    update local-nuget.bat

--- local-nuget.bat
+++ local-nuget.bat
@@ -1,6 +1,13 @@
+dotnet nuget enable source debug-repository
 dotnet nuget add source local-nuget -n debug-repository
-dotnet nuget push ..\FluentCeVIOWrapper\FluentCeVIOWrapper.Common\bin\Release\FluentCeVIOWrapper.0.1.1.nupkg -s debug-repository
+dotnet nuget push ..\FluentCeVIOWrapper\FluentCeVIOWrapper.Common\bin\Release\FluentCeVIOWrapper.0.1.3.nupkg -s debug-repository
 dotnet nuget list source
 
 rem dotnet nuget remove source debug-repository
 rem dotnet nuget disable source debug-repository
+
+cd KuchiPaku
+dotnet add package FluentCeVIOWrapper
+cd ../KuchiPaku.Core
+dotnet add package FluentCeVIOWrapper
+cd ../
