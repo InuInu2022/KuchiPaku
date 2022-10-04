ba95985d0a92de174f48a31b789880d20e68e25d
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2022-10-05 00:26:32 +0900

    update copy bat

--- copy-server.bat
+++ copy-server.bat
@@ -1,1 +1,3 @@
 copy ..\FluentCeVIOWrapper\FluentCeVIOWrapper.Server\bin\Release\net48\FluentCeVIOWrapper.Server.exe KuchiPaku\Server
+copy ..\FluentCeVIOWrapper\FluentCeVIOWrapper.Server\bin\Release\net48\FluentCeVIOWrapper.Server.exe.config KuchiPaku\Server
+copy ..\FluentCeVIOWrapper\FluentCeVIOWrapper.Server\bin\Release\net48\*.dll KuchiPaku\Server
