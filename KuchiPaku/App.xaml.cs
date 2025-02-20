a8285fff7ea71f4b3028b94f67ec44d9a56d81bf
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-02-20 16:47:27 +0900

    feat: adjust UI elements in MainWindow for improved layout and add tooltip functionality for images

--- KuchiPaku/App.xaml.cs
+++ KuchiPaku/App.xaml.cs
@@ -10,10 +10,10 @@ using System.Windows;
 
 namespace KuchiPaku
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
     }
 }
